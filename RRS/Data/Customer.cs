2c940d9996268b4b1f174f482e7537a9f8ad8ed9
Joshua Zimbicki <Josh8011@yahoo.com>
2022-04-06 12:17:10 +1000

    number

--- RRS/Data/Customer.cs
+++ RRS/Data/Customer.cs
@@ -5,12 +5,9 @@
         public List<Reservation> Reservations { get; set; }
         public bool IsAVIP { get; set; }
         public bool IsNotAVIP { get; set; }
-
         public string Hello1 = "Yo";
-
         public string Hello2 = "Hello from Atif!";
         public int something { get; set; }
-
         public int number = 123;
     }
 }
