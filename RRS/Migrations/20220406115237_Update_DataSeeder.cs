4c756f5b7c69786b3acf4cc8d2f1da541c891b8e
Mason-D <masonglendixon@gmail.com>
2022-04-06 22:00:57 +1000

    Updated Migration

---
+++ RRS/Migrations/20220406115237_Update_DataSeeder.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RRS.Migrations
+{
+    public partial class Update_DataSeeder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNo",
+                table: "People",
+                newName: "PhoneNumber");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Restaurants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Restaurants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Restaurants",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerNotes",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxFileNumber",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVIP",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Address", "Email", "PhoneNumber" },
+                values: new object[] { "123 Bean St, Sydney", "Bean@Scene.com", "123-456-789" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerNotes",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "TaxFileNumber",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "IsVIP",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "People",
+                newName: "PhoneNo");
+        }
+    }
+}
