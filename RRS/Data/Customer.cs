39a18680e0612bbd8f50ca09d593faf23376771f
Joshua Zimbicki <Josh8011@yahoo.com>
2022-04-06 12:29:23 +1000

    Change back

--- RRS/Data/Customer.cs
+++ RRS/Data/Customer.cs
@@ -3,11 +3,6 @@
     public class Customer : Person
     {
         public List<Reservation> Reservations { get; set; }
-        public bool IsAVIP { get; set; }
-        public bool IsNotAVIP { get; set; }
-        public string Hello1 = "Yo";
-        public string Hello2 = "Hello from Atif!";
-        public int something { get; set; }
-        public int number1 = 123;
-        public int number2 = 456;    }
+        public bool IsVIP { get; set; }
+    }
 }
