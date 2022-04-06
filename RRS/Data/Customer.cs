d95aee89ffca6844c20a2b2d93315cb348aae146
Atif <atif.onat@studytafensw.edu.au>
2022-04-06 11:54:36 +1000

    Added IsNotAVip property & Hello string to Customer table.

--- RRS/Data/Customer.cs
+++ RRS/Data/Customer.cs
@@ -3,6 +3,9 @@
     public class Customer : Person
     {
         public List<Reservation> Reservations { get; set; }
-        public bool IsVIP { get; set; }
+
+        public bool IsNotAVIP { get; set; }
+
+        public string Hello = "Hello from Atif!";
     }
 }
