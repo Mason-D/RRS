15c5097e2be70fec0f74b919c27e892b33019197
Atif <atif.onat@studytafensw.edu.au>
2022-04-28 13:11:35 +1000

    Flattened Sitting StartTime further in SittingDto to include Year, Month, Day. Removed End time.

--- RRS/Models/SittingDto.cs
+++ RRS/Models/SittingDto.cs
@@ -4,8 +4,10 @@
     {
         public int Id { get; set; }
         public DateTime Start { get; set; }
+        public int Year { get => Start.Year; }
+        public int Month { get => Start.Month; }
+        public int Day { get => Start.Day; }
         public int Duration { get; set; }
-        public DateTime End { get => Start.AddMinutes(Duration); }
         public bool IsOpen { get; set; }
         public int Capacity { get; set; }
         public string SittingTypeDescription { get; set; }
