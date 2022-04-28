0faef90c40698a0008b0a62c4e703168111363f8
Atif <atif.onat@studytafensw.edu.au>
2022-04-28 12:19:36 +1000

    Seperated api controllers

--- RRS/Controllers/API/ReservationController.cs
+++ RRS/Controllers/API/ReservationController.cs
@@ -16,27 +16,6 @@ namespace RRS.Controllers.API
             _context = context;
         }
 
-        // Reservation/People
-        [HttpGet]
-        public ActionResult<IEnumerable<SittingDto>> GetSittings()
-        {
-            var t2 = _context.Sittings
-                        .Where(s => s.IsOpen)
-                        .Select(s => new SittingDto
-                        {
-                            Id = s.Id,
-                            Start = s.Start,
-                            Duration = s.Duration,
-                            Capacity = s.Capacity,
-                            IsOpen = s.IsOpen,
-                            SittingTypeDescription = s.SittingType.Description
-                        })
-                        .ToList();
-
-            return t2;
-        }
-
-        // Reservation/Details
         [HttpPost]
         public ReservationDto CreateReservation(ReservationDto resDTO)
         {
