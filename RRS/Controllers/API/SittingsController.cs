b6fb5e03565717e948c4fc292add781a1b524dc0
Atif <atifonat@gmail.com>
2022-04-29 11:28:11 +1000

    Sittings api now has an Available(start, end) action.

--- RRS/Controllers/API/SittingsController.cs
+++ RRS/Controllers/API/SittingsController.cs
@@ -17,74 +17,48 @@ namespace RRS.Controllers.API
         }
 
         [HttpGet]
-        [Route("all")]
-        public ActionResult<IEnumerable<SittingDto>> All()
+        [Route("available/{start}/{end?}")]
+        public ActionResult<IEnumerable<SittingDto>> Available(DateTime start, DateTime end)
         {
-            return _context.Sittings
-                        .Where(s => s.IsOpen)
-                        .Select(s => new SittingDto
-                        {
-                            Id = s.Id,
-                            Start = s.Start,
-                            Duration = s.Duration,
-                            Capacity = s.Capacity,
-                            IsOpen = s.IsOpen,
-                            SittingTypeDescription = s.SittingType.Description
-                        })
-                        .ToList();
+            var startLocal = start.ToLocalTime();
+
+            if (end == new DateTime()) // Get 'All' available sittings
+            {
+                return _context.Sittings
+                            .Where(s => s.IsOpen
+                                        && s.Start > startLocal)
+                            .Select(s => new SittingDto
+                            {
+                                Id = s.Id,
+                                Start = s.Start,
+                                Duration = s.Duration,
+                                Capacity = s.Capacity,
+                                IsOpen = s.IsOpen,
+                                SittingTypeDescription = s.SittingType.Description
+                            })
+                            .ToList();
+            }
+            else // Get sittings within start <-> end range
+            {
+                var endLocal = end.ToLocalTime();
+
+                return _context.Sittings
+                            .Where(s => s.IsOpen)
+                            .Where(s => s.Start >= startLocal
+                                        && s.Start < endLocal)
+                            .Select(s => new SittingDto
+                            {
+                                Id = s.Id,
+                                Start = s.Start,
+                                Duration = s.Duration,
+                                Capacity = s.Capacity,
+                                IsOpen = s.IsOpen,
+                                SittingTypeDescription = s.SittingType.Description
+                            })
+                            .ToList();
+            }
         }
 
-        [HttpGet]
-        [Route("available/{year}-{month}-{day}")]
-        //HACK, use iso-string
-        public ActionResult<IEnumerable<SittingDto>> Available(int year, int month, int day)
-        {
-            var startDate = new DateTime(year, month, day);
-            var endDate = startDate.AddDays(29); //Excludes sittings on 29th day
-
-            return _context.Sittings
-                        .Where(s => s.IsOpen)
-                        .Where(s => DateTime.Compare(s.Start, startDate) >= 0 && DateTime.Compare(s.Start, endDate) <= 0)
-                        .Select(s => new SittingDto
-                        {
-                            Id = s.Id,
-                            Start = s.Start,
-                            Duration = s.Duration,
-                            Capacity = s.Capacity,
-                            IsOpen = s.IsOpen,
-                            SittingTypeDescription = s.SittingType.Description
-                        })
-                        .ToList();
-        }
-
-        [HttpGet]
-        [Route("available-soon/{earliest}")]
-        //NOT IMPLEMENTED
-        public ActionResult<IEnumerable<SittingDto>> AvailableSoon(DateTime earliest)
-        {
-            return _context.Sittings
-                        .Where(s => s.IsOpen)
-                        .Where(s => isAvailableSoon(s.Start, earliest))
-                        .Select(s => new SittingDto
-                        {
-                            Id = s.Id,
-                            Start = s.Start,
-                            Duration = s.Duration,
-                            Capacity = s.Capacity,
-                            IsOpen = s.IsOpen,
-                            SittingTypeDescription = s.SittingType.Description
-                        })
-                        .ToList();
-        }
-
-        private bool isAvailableSoon(DateTime sitting, DateTime earliest)
-        {
-            return sitting >= earliest
-                && sitting.Year == earliest.Year 
-                && (sitting.Month == earliest.Month || sitting.Month == earliest.Month + 1);
-        }
-
-        // Returns all days in month that have any open sittings 
         [HttpGet]
         [Route("distinct-available/{month}")]
         public ActionResult<IEnumerable<DateTime>> DistinctAvailable(int month)
