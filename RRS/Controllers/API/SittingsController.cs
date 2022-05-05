e3cd0c29032580a1f156b23f83cd3b957e32d677
Atif <atif.onat@studytafensw.edu.au>
2022-05-05 15:41:57 +1000

    Version 1, nested dictionary in distinct available sittings api

--- RRS/Controllers/API/SittingsController.cs
+++ RRS/Controllers/API/SittingsController.cs
@@ -41,36 +41,77 @@ namespace RRS.Controllers.API
 
         [HttpGet]
         [Route("distinct-available/{start}/{end?}")]
-        public ActionResult<Dictionary<int, List<int>>> DistinctAvailable(DateTime start, DateTime end = new DateTime())
+        public ActionResult<Dictionary<int, List<Dictionary<int, List<int>>>>> DistinctAvailable(DateTime start, DateTime end = new DateTime())
         {          
             var startLocal = start.ToLocalTime();
             var endLocal = end == new DateTime() ? startLocal.AddDays(28 * 3) : end.ToLocalTime();
-            var dict = new Dictionary<int, List<int>>();
+            var yearMonthDict = new Dictionary<int, List<int>>();
+            var monthDayDict = new Dictionary<int, List<int>>();
 
             var sittings = _context.Sittings
                         .Where(s => s.IsOpen
                                     && s.Start >= startLocal
                                     && s.Start < endLocal)
-                        .Select(s => new { Month = s.Start.Month, Day = s.Start.Day })
+                        .Select(s => new { Year = s.Start.Year, Month = s.Start.Month, Day = s.Start.Day })
                         .ToList();
 
             foreach (var sitting in sittings)
             {
-                if (dict.ContainsKey(sitting.Month))
+
+                if (yearMonthDict.ContainsKey(sitting.Year))
                 {
-                    if (!dict[sitting.Month].Contains(sitting.Day))
+                    if (!yearMonthDict[sitting.Year].Contains(sitting.Month))
                     {
-                        dict[sitting.Month].Add(sitting.Day);
+                        yearMonthDict[sitting.Year].Add(sitting.Month);
                     }
                 }
                 else
                 {
-                    dict.Add(sitting.Month, new List<int>());
-                    dict[sitting.Month].Add(sitting.Day);
+                    yearMonthDict.Add(sitting.Year, new List<int>());
+                    yearMonthDict[sitting.Year].Add(sitting.Month);
                 }
             }
 
-            return dict;
+            foreach (var sitting in sittings)
+            {
+                if (monthDayDict.ContainsKey(sitting.Month))
+                {
+                    if (!monthDayDict[sitting.Month].Contains(sitting.Day))
+                    {
+                        monthDayDict[sitting.Month].Add(sitting.Day);
+                    }
+                }
+                else
+                {
+                    monthDayDict.Add(sitting.Month, new List<int>());
+                    monthDayDict[sitting.Month].Add(sitting.Day);
+                }
+            }
+
+            var fusedDict = new Dictionary<int, List<Dictionary<int, List<int>>>>();
+
+            foreach (var yearMonth in yearMonthDict)
+            {
+                foreach (var monthDay in monthDayDict)
+                {
+                    if (fusedDict.TryGetValue(yearMonth.Key, out var oldMonthDayDict))
+                    {
+                        var temp = new Dictionary<int, List<int>>();
+                        temp.Add(monthDay.Key, monthDay.Value);
+
+                        fusedDict[yearMonth.Key].Add(temp);
+                    }
+                    else
+                    {
+                        var temp1 = new List<Dictionary<int, List<int>>>();
+                        var temp2 = new Dictionary<int, List<int>>();
+                        temp2.Add(monthDay.Key, monthDay.Value);
+                        fusedDict.Add(yearMonth.Key, temp1);
+                    }
+                }
+            }
+
+            return fusedDict;
         }
 
         [HttpGet]
