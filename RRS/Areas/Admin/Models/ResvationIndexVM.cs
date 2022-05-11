2d39ec3d2717218f1f1e9887c0a9498a31a45959
Mason-D <masonglendixon@gmail.com>
2022-05-12 03:01:14 +1000

    add model

--- RRS/Areas/Admin/Models/ResvationIndexVM.cs
+++ RRS/Areas/Admin/Models/ResvationIndexVM.cs
@@ -1,6 +1,14 @@
 ﻿namespace RRS.Areas.Admin.Models
 {
-    public class ResvationIndexVM
+    public class EditReservationVM
     {
+        public int ReservationId { get; set; }
+        public int NoOfGuests { get; set; }
+        public int CustomerId { get; set; }
+        public int SittingId { get; set; }
+        public int ReservationStatusId { get; set; }
+        public int ReservationOriginId { get; set; }
+        public string CustomerNotes { get; set; }
+
     }
 }
