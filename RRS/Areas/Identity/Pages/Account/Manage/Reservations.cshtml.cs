aa755b80df28a68e88a67f9b907279cfd30888a2
Atif <atifonat@gmail.com>
2022-05-11 11:20:19 +1000

    Updated past & upcoming reservations  profile display functionality

--- RRS/Areas/Identity/Pages/Account/Manage/Reservations.cshtml.cs
+++ RRS/Areas/Identity/Pages/Account/Manage/Reservations.cshtml.cs
@@ -23,39 +23,43 @@ namespace RRS.Areas.Identity.Pages.Account.Manage
             _context = context;
         }
 
-        public List<Reservation> Reservations { get; set; }
+        public List<Reservation>? UpcomingReservations { get; set; }
+        public List<Reservation>? PastReservations { get; set; }
 
         [BindProperty]
         public InputModel Input { get; set; }
 
         public class InputModel
         {
-            public List<Reservation>? UpcomingReservations { get; set; }
-            public List<Reservation>? PastReservations { get; set; }
-        }
-        private async Task LoadAsync(IdentityUser user)
-        {          
-            var currentDate = DateTime.Now;
-            var reservations = await _context.Reservations.Include(r => r.Sitting).Where(r => "93a658ff-1307-471d-a810-695a7a76587c" == user.Id).ToListAsync();
-            var past = reservations.Where(r => r.Sitting.Start < currentDate).ToList();
-            var upcoming = reservations.Where(r => r.Sitting.Start >= currentDate).ToList();
 
-            Input = new InputModel
-            {
-                PastReservations = past,
-                UpcomingReservations = upcoming
-            };
         }
 
         public async Task<IActionResult> OnGetAsync()
         {
             var user = await _userManager.GetUserAsync(User);
+
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            await LoadAsync(user);
+            var currentDate = DateTime.Now;
+            var customer = await _context.People.Where(p => p.UserId == user.Id).FirstOrDefaultAsync();
+            var reservations = await _context.Reservations
+                .Include(r => r.Customer)
+                .Where(r => r.CustomerId == customer.Id)
+                .Include(r => r.Sitting)
+                    .ThenInclude(s => s.SittingType)
+                .Include(r => r.ReservationOrigin)
+                .Include(r => r.ReservationStatus)
+                .ToListAsync();
+
+            var past = reservations.Where(r => r.Sitting.Start < currentDate).ToList();
+            var upcoming = reservations.Where(r => r.Sitting.Start >= currentDate).ToList();
+
+            PastReservations = past;
+            UpcomingReservations = upcoming;
+
             return Page();
         }
 
