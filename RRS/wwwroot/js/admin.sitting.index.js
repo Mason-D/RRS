b83478f72ff08015a64aa55dae33c0f901d957a8
Atif <atif.onat@studytafensw.edu.au>
2022-05-12 12:28:06 +1000

    check box works, needs to be fixed to allow singular check

--- RRS/wwwroot/js/admin.sitting.index.js
+++ RRS/wwwroot/js/admin.sitting.index.js
@@ -19,16 +19,38 @@
                             <td>${item.isOpen}</td>
                             <td>${item.sittingTypeDescription}</td>
                             <td>(Not yet in entity)</td>
-                            <td><input type="checkbox" data-sitting-id="${item.id}" class="radio select-sitting-cb " value="${index}" id="type${index}" name="type${index}" /></td>
+                            <td>
+                                <input
+                                    type="checkbox"
+                                    data-sitting-id="${item.id}"
+                                    data-sitting-capacity="${item.capacity}"
+                                    data-sitting-isOpen="${item.isOpen}"
+                                    data-sitting-typeDescription="${item.sittingTypeDescription}"
+                                    class="radio select-sitting-cb "
+                                    value="${index}" 
+                                    id="type${index}" 
+                                    name="type${index}" />
+                            </td>
                         </tr>`
                     );
                     $("#sittingsTBody").find('.select-sitting-cb').click((e) => {
-                        let x = $(e.target).data('sitting-id');
+                        //let x = $(e.target).data('sitting-id');
+                        //$("#idInput").val(`${item.id}`)
+                        //$("#startInput").val(`${item.start}`)
+                        //$("#durationInput").val(`${item.isOpen}`)
+                        //$("#capacityInput").val(`${item.sittingTypeDescription}`)
                     });
 
 
                 })
 
+            });
+
+            $("#sittingsTBody").on('change', "input[type='checkbox']", function (e) {
+                let id = $(e.target).data('sittingId');
+                let capacity = $(e.target).data('sittingCapacity');
+                let isOpen = $(e.target).data('sittingIsOpen');
+                let typeDescription = $(e.target).data('sittingTypeDescription');
             });
         });
     });
