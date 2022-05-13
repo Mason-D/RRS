d2e3167614cd80a2de99ef7e99c658d7d03069a4
Atif <atifonat@gmail.com>
2022-05-13 10:01:06 +1000

    removed attributes namespace

--- RRS/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RRS/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using RRS.Attributes;
 using RRS.Data;
 using RRS.Models;
 using RRS.Services;
