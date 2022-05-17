743f512704925417f007cc6aded09f869c48aefe
Atif <atifonat@gmail.com>
2022-05-17 23:50:13 +1000

    return edit rather then not found /Edit/Obj

--- RRS/Areas/Admin/Controllers/SittingController.cs
+++ RRS/Areas/Admin/Controllers/SittingController.cs
@@ -96,7 +96,7 @@ namespace RRS.Areas.Admin.Controllers
         {
             if (sittingDto.Id <= 0)
             {
-                return NotFound();
+                return View("Edit");
             }
 
             if (ModelState.IsValid)
@@ -133,12 +133,6 @@ namespace RRS.Areas.Admin.Controllers
                 }
                 return RedirectToAction(nameof(Edit), "Sitting", new { LastSelectedDate = sittingDto.Start.ToString("yyyy-MM-dd") });
             }
-            //ViewData["SittingTypeId"] = new SelectList(_context.SittingTypes, "Id", "Description");
-            //if (ModelState["Start"].ValidationState.ToString() == "Valid")
-            //{
-            //    return RedirectToAction(nameof(Edit), "Sitting", new { LastSelectedDate = sittingDto.Start.ToString("yyyy-MM-dd") });
-            //}
-            //return View("Edit");
             ViewData["LastSelectedDate"] = ModelState["Start"].ValidationState.ToString() == "Valid" ? sittingDto.Start.ToString("yyyy-MM-dd") : null;
             ViewData["SittingTypeId"] = new SelectList(_context.SittingTypes, "Id", "Description");
             return View("Edit");
