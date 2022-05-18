944e521de6fa951f8ff0080d123271964ad96743
Atif <atifonat@gmail.com>
2022-05-18 11:49:50 +1000

    Table rows selectable (highlights), selected checkbox removed & column removed.

--- RRS/wwwroot/js/admin.sitting.index.js
+++ RRS/wwwroot/js/admin.sitting.index.js
@@ -19,6 +19,27 @@
             }
         }
     });
+
+    $("#sittingsTBody").on('click', 'tr', function (e) {
+
+        onRowSelect(this);
+
+        //Posted
+        $("#hiddenIdInput").val(`${$(e.currentTarget).data('sitting-id')}`);
+        $("#hiddenTypeIdInput").val(`${$(e.currentTarget).data('sitting-type-id')}`);
+        $("#startInput").val(`${$(e.currentTarget).data('sitting-start')}`);
+        $("#durationInput").val(`${$(e.currentTarget).data('sitting-duration')}`);
+        $("#capacityInput").val(`${$(e.currentTarget).data('sitting-capacity')}`);
+        $("#typeSelectList").val(`${$(e.currentTarget).data('sitting-type-id')}`);
+        // Match checkbox state to selected sitting isOpen value
+        let open = true == $(e.currentTarget).data('sitting-is-open'); //String to bool
+        $("#isOpenInput").prop('checked', open);
+        $("#isOpenInput").val(`${open}`);
+        //Posted End
+
+        //Purely Visual
+        $("#idInput").val(`${$(e.currentTarget).data('sitting-id')}`);
+    });
 });
 
 function populateSittingsTable(newDate) {
@@ -32,55 +53,32 @@ function populateSittingsTable(newDate) {
             let formattedStart = new Date(item.start).toLocaleTimeString("en-US");
 
             $("#sittingsTBody").append(
-                `<tr>
-                    <td>${item.id}</td>
-                    <td>${formattedStart}</td>
-                    <td>${item.duration}</td>
-                    <td>${item.capacity}</td>
-                    <td>${item.isOpen}</td>
-                    <td>${item.sittingTypeDescription}</td>
-                    <td>(Not yet in entity)</td>
-                    <td>
-                        <input
-                            type="checkbox"
-                            data-sitting-id="${item.id}"
-                            data-sitting-start="${item.start}"
-                            data-sitting-duration="${item.duration}"
-                            data-sitting-capacity="${item.capacity}"
-                            data-sitting-is-open="${item.isOpen}"
-                            data-sitting-type-id="${item.sittingTypeId}"
-                            /*data-sitting-type-description="${item.sittingTypeDescription}"*/
-                            class="radio select-sitting-cb "
-                            value="${index}" 
-                            id="type${index}" 
-                            name="type${index}" />
-                    </td>
+                `<tr    
+                    data-sitting-id="${item.id}"
+                    data-sitting-start="${item.start}"
+                    data-sitting-duration="${item.duration}"
+                    data-sitting-capacity="${item.capacity}"
+                    data-sitting-is-open="${item.isOpen}"
+                    data-sitting-type-id="${item.sittingTypeId}"
+                    data-sitting-type-description="${item.sittingTypeDescription}"
+                    class="radio select-sitting-cb "
+                    value="${index}" 
+                    id="type${index}" 
+                    name="type${index}">
+
+                        <td>${item.id}</td>
+                        <td>${formattedStart}</td>
+                        <td>${item.duration}</td>
+                        <td>${item.capacity}</td>
+                        <td>${item.isOpen}</td>
+                        <td>${item.sittingTypeDescription}</td>
+                        <td>(Not yet in entity)</td>
+
                 </tr>`
             );
             /*$("#sittingsTBody").find('.select-sitting-cb').click((e) => {});*/
         })
 
-    });
-
-    $("#sittingsTBody").on('change', "input[type='checkbox']", function (e) {
-
-        $("#sittingsTBody input[type='checkbox']").not(e.target).prop('checked', false);
-
-        //Posted
-        $("#hiddenIdInput").val(`${$(e.target).data('sitting-id')}`);
-        $("#hiddenTypeIdInput").val(`${$(e.target).data('sitting-type-id')}`);
-        $("#startInput").val(`${$(e.target).data('sitting-start')}`);
-        $("#durationInput").val(`${$(e.target).data('sitting-duration')}`);
-        $("#capacityInput").val(`${$(e.target).data('sitting-capacity')}`);
-        $("#typeSelectList").val(`${$(e.target).data('sitting-type-id')}`);
-        // Match checkbox state to selected sitting isOpen value
-        let open = true == $(e.target).data('sitting-is-open'); //String to bool
-        $("#isOpenInput").prop('checked', open);
-        $("#isOpenInput").val(`${open}`);
-        //Posted End
-
-        //Purely Visual
-        $("#idInput").val(`${$(e.target).data('sitting-id')}`);
     });
 };
 
@@ -92,5 +90,15 @@ function clearForm() {
     $("#capacityInput").val("");
     $("#isOpenInput").val("");
     $("#idInput").val("");
+}
+
+function onRowSelect(row) {
+    //Deselect any highlighted rows
+    $('#sittingsTBody').find('.bg-primary').removeClass('bg-primary');
+    $('#sittingsTBody').find('.text-white').removeClass('text-white');
+    console.log(temp);
+    //Highlight selected row
+    $(row).addClass('bg-primary');
+    $(row).addClass('text-white');
 }
 
