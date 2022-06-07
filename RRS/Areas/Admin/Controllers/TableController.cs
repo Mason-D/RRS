6e266b4cc4f33418e29167fbe61b8649c861c14f
Joshua Zimbicki <Josh8011@yahoo.com>
2022-06-07 11:18:16 +1000

    added tablel it gross

--- RRS/Areas/Admin/Controllers/TableController.cs
+++ RRS/Areas/Admin/Controllers/TableController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RRS.Data;
 
 namespace RRS.Areas.Admin.Controllers
 {
-    public class TableController : Controller
+    public class TableController : AdminAreaController
     {
+        public TableController(ApplicationDbContext context, UserManager<IdentityUser> userManager) : base(context, userManager)
+        {
+        }
+
         public IActionResult Index()
         {
             return View();
